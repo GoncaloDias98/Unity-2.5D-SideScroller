981ba029c27224ece7fc7b3d24bd4676ed8984df
Gonçalo Dias <32722225+GoncaloDias98@users.noreply.github.com>
2021-05-13 22:41:28 +0100

    Initial commit

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour{
+  
+    private Transform target;
+    private float trackSpeed = 10;
+
+    public void SetTarget(Transform t){
+        target = t;
+    }
+
+    void LateUpdate() {
+        if(target){
+            float x =IncrementTowards(transform.position.x, target.position.x, trackSpeed);
+            float y =IncrementTowards(transform.position.y, target.position.y, trackSpeed);
+            transform.position = new Vector3(x,y,transform.position.z);
+        }
+    }
+
+private float IncrementTowards(float n, float target, float a){
+        if(n == target){ //se o speed for igual ao target, retorna o speed
+            return n;
+        }else{
+            float dir = Mathf.Sign(target - n); //
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target-n))? n : target; // se o speed passar o target entao retorna o target senao retorna o speed
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent (typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+    public LayerMask collisionMask;
+
+    private BoxCollider collider;
+    private Vector3 s;
+    private Vector3 c;
+
+    private Vector3 originalSize;
+    private Vector3 originalCentre;
+    private float colliderScale;
+
+    private int collisionDivisionX = 3;
+    private int collisionDivisionY = 10;
+    private float skin = .005f;
+
+    [HideInInspector]
+    public bool grounded;
+
+    [HideInInspector]
+    public bool movementStopped;
+
+    Ray ray;
+    RaycastHit hit;
+
+    void Start(){
+        collider = GetComponent<BoxCollider>();
+        colliderScale = transform.localScale.x;
+        originalSize = collider.size;
+        originalCentre = collider.center;
+        SetCollider(originalSize,originalCentre);
+    }
+
+    public void Move(Vector2 moveAmount){
+        float deltaY = moveAmount.y;
+        float deltaX = moveAmount.x;
+        Vector2 p = transform.position;
+
+//Colisoes cima e baixo
+        grounded = false;
+        for(int i = 0; i<collisionDivisionX; i++){
+            float dir = Mathf.Sign(deltaY);
+            float x =(p.x + c.x - s.x/2) + s.x/(collisionDivisionX-1) * i; //ponto esquerdo, direito e centro da base do collider
+            float y = p.y + c.y +s.y/2 * dir; //base do collider
+
+            ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+            Debug.DrawRay(ray.origin,ray.direction);
+            if(Physics.Raycast(ray,out hit, Mathf.Abs(deltaY) + skin,collisionMask)){
+                float dst = Vector3.Distance (ray.origin,hit.point);
+
+                if(dst > skin){ //Para a queda do jogador se tocar no colider
+                    deltaY = dst * skin - skin * dir;
+                }else{
+                    deltaY = 0;
+                }
+                grounded = true;
+                break;
+            }
+        }
+
+//Colisoes direita e esquerda
+    movementStopped = false;
+        for(int i = 0; i<collisionDivisionY; i++){
+            float dir = Mathf.Sign(deltaX);
+            float x =p.x + c.x + s.x/2 * dir; //ponto esquerdo, direito e centro da base do collider
+            float y = p.y + c.y - s.y/2 + s.y/(collisionDivisionY-1) * i; //base do collider
+
+            ray = new Ray(new Vector2(x,y), new Vector2(dir,0));
+            Debug.DrawRay(ray.origin,ray.direction);
+            if(Physics.Raycast(ray,out hit, Mathf.Abs(deltaX) + skin,collisionMask)){
+                float dst = Vector3.Distance (ray.origin,hit.point);
+
+                if(dst > skin){ //Para a queda do jogador se tocar no colider
+                    deltaX = dst * skin - skin * dir;
+                }else{
+                    deltaX = 0;
+                }
+                movementStopped =true;
+                break;
+            }
+        }
+
+        if(!grounded && !movementStopped){
+        Vector3 playerDir = new Vector3(deltaX,deltaY);
+        Vector3 o = new Vector3(p.x +c.x+s.x/2*Mathf.Sign(deltaX), p.y+c.y+s.y/2*Mathf.Sign(deltaY));
+        
+        ray = new Ray(o,playerDir.normalized);
+        if(Physics.Raycast(ray,Mathf.Sqrt(deltaX*deltaX+deltaY*deltaY))){
+            grounded=true;
+            deltaY=0;
+        }
+        }
+        
+
+        Vector2 finalTransform = new Vector2(deltaX,deltaY);
+        transform.Translate(finalTransform,Space.World);
+    }
+   
+   public void SetCollider(Vector3 size, Vector3 centre){
+       collider.size = size;
+       collider.center = centre;
+
+       s = size * colliderScale;
+       c = centre *colliderScale;
+   }
+
+   public void ResetCollider(){
+       SetCollider(originalSize,originalCentre);
+   }
+
+}
